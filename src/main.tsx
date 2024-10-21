5a9c8f6b64bf9d8c0c6b0c63d0f4e25c6e6a6b7b
Imad Ouguaour <imad.ouguaour-ext@aphp.fr>
2024-10-21 21:42:26 +0200

    config to deploy on github-pages

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ import store from './store.ts';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename='/StreamShow'>
         <App />
       </BrowserRouter>
     </Provider>
